d8ffce8ad0ddfb1a3b29bd641b03c1789e438686
Afek <afek@sms2010.co.il>
2021-01-25 12:55:44 +0200

    Created a new Header file wuth new components

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+
+const Header = ({ title }) => {
+  // instead of using props  and prop.TheNameOfTheProperty you can destructure with curly braces like shown
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+/*This is how you can make your code more robust 
+and you can define the type of your propertie 
+and if they are required or not */
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS (inline) - In order to use headingStyle you write "style={headingStyle}" as an inline attrubute in the html element you want to have this style. <h1 style={headingStyle}></h1>
+/*
+const headingStyle = {
+    color: 'red',
+    bacgroundColor: 'blue',
+} 
+*/
+
+export default Header;
