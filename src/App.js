87ba25813a0737292156adc8cb84e067d07edf9f
Afek <afek@sms2010.co.il>
2021-01-25 17:26:12 +0200

    Added the tasks object to the App component so other componewnts that i need would be abble to work with it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
-function App() {
+const App = () => {
+  const tasks = [
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 4:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Sooper-Market",
+      day: "Feb 5th at 9:30pm",
+      reminder: false,
+    },
+  ];
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
-}
+};
 
 export default App;
