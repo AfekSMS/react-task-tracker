c858a29376d556fd1555f1c1727465023143bec1
Afek <afek@sms2010.co.il>
2021-01-26 18:53:17 +0200

    moved the task list to the db.json wich simulates an api / database

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
   const [showAddTask, setshowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 4:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Sooper-Market",
-      day: "Feb 5th at 9:30pm",
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
+
+  //Fetch Taska
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    console.log(data);
+    return data;
+  };
 
   //Add Task
   const addTask = (task) => {
@@ -34,7 +32,8 @@ const App = () => {
   };
 
   //Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "Delete" });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
