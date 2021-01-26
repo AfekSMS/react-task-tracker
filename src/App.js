e8c3bb09bf62539c49e278b52fe0d73f8da4d9f7
Afek <afek@sms2010.co.il>
2021-01-26 11:46:56 +0200

    added two function components that handel the delete event  and the toggle true/false event of a task and added a no task to show message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 const App = () => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       id: 1,
       text: "Doctors Appointment",
@@ -22,12 +22,31 @@ const App = () => {
       day: "Feb 5th at 9:30pm",
       reminder: false,
     },
-  ];
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const ToggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+    console.log(id);
+  };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={ToggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 };
