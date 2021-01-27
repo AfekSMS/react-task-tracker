3f720f36e283a741850cb5b8830d47a38f72c20e
Afek <afek@sms2010.co.il>
2021-01-27 10:06:56 +0200

    update the code to work with the db with fetch

--- src/App.js
+++ src/App.js
@@ -16,19 +16,32 @@ const App = () => {
     getTasks();
   }, []);
 
-  //Fetch Taska
+  //Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch("http://localhost:5000/tasks");
     const data = await res.json();
-    console.log(data);
+    return data;
+  };
+
+  //Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
     return data;
   };
 
   //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
   };
 
   //Delete Task
@@ -38,13 +51,23 @@ const App = () => {
   };
 
   //Toggle Reminder
-  const ToggleReminder = (id) => {
+  const ToggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.ToggleReminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+    const data = await res.json();
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
-    console.log(id);
   };
 
   return (
